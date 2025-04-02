9e24efbd6511e42b5cf9bad63d08d0bc49d6ff2d
abhigyantripathi <abhigyantripathi2021@gmail.com>
2025-04-02 12:45:20 +0530

    swiper

--- script.js
+++ script.js
@@ -25,13 +25,13 @@ function page4Animation() {
     })
 }
 
-function swiperAnimation() {
-    var swiper = new Swiper(".mySwiper", {
-        slidesPerView: "auto",
-        centeredSlides: true,
-        spaceBetween: 100,
-    });
-}
-
-swiperAnimation()
+// var swiper = new Swiper(".mySwiper", {
+//     slidesPerView: "auto",
+//     centeredSlides: true,
+//     spaceBetween: 30,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//   });
 page4Animation()
